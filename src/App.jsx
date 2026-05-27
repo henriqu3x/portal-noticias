f2443bbd35e1220403ee4ce41df530bc3b6680da
Luiz Henrique <163440449+henriqu3x@users.noreply.github.com>
2026-05-27 15:00:48 -0300

    add header html

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
 const App = () => {
   return(
     <>
+      <header>
+        <h2>Portal</h2>
+        <nav>
+          <ul>
+            <li>
+              <a href="" aria-label="Noticias-recentes">Noticias recentes</a>
+            </li>
+            <li>
+              <a href="" aria-label="Principais-noticias">Principais noticias</a>
+            </li>
+          </ul>
+        </nav>
+        <form>
+          <input type="text" placeholder="Pesquisar"/>
+          <button aria-label="Pesquisar Conteudo"><i class="fa-solid fa-magnifying-glass"></i></button>
+        </form>
+      </header>
     </>
   )
 }
