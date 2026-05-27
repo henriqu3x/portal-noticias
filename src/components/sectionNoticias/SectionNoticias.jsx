204b7a0fb974f7e14be12edfc0b8ebfa2c4eabb7
henriqu3x <163440449+henriqu3x@users.noreply.github.com>
2026-05-27 20:56:16 -0300

    estilizacao section noticias css

--- src/components/sectionNoticias/SectionNoticias.jsx
+++ src/components/sectionNoticias/SectionNoticias.jsx
@@ -1,35 +1,38 @@
 import './sectionNoticias.css'
+import imgNoticia4 from '../../assets/noticia4.avif'
+import imgNoticia5 from '../../assets/noticia5.avif'
 
 const SectionNoticias = () => {
     return (
         <section aria-label="Sessão Noticias" id="noticias">
             <aside id="noticias-laterais">
                 <article>
-                    <img src="" alt="" />
-                    <h2>title</h2>
-                    <p>descricao</p>
+                    <h2>Deputados debatem fim da escala 6x1</h2>
+                    <p>Comissão especial aprovou proposta por 34 votos a 4</p>
                 </article>
                 <article>
-                    <img src="" alt="" />
-                    <h2>title</h2>
-                    <p>descricao</p>
+                    <h2>Justiça suspende 'times square paulistana'</h2>
+                    <p>Prefeitura de São Paulo pode apresentar recurso</p>
                 </article>
                 <article>
-                    <img src="" alt="" />
-                    <h2>title</h2>
-                    <p>descricao</p>
+                    <h2>Papa Leão XIV conhece Ferrari Luce de R$ 3,2 milhões e ganha volante</h2>
+                    <p>a Luce é o primeiro modelo da marca italiana com espaço para cinco ocupantes</p>
                 </article>
             </aside>
             <section aria-label="noticias principais" id="noticias-principais">
                 <article>
-                    <img src="" alt="" />
-                    <h2>title</h2>
-                    <p>descricao</p>
+                    <img src={imgNoticia4} alt="" />
+                    <div role='group'>
+                        <h2>Mulher jogada em penhasco pelo ex se escondeu da equipe de buscas por achar que agressor havia retornado</h2>
+                        <p>De acordo com os bombeiros, a vítima só percebeu que se tratava de ajuda no dia seguinte</p>
+                    </div>
                 </article>
                 <article>
-                    <img src="" alt="" />
-                    <h2>title</h2>
-                    <p>descricao</p>
+                    <img src={imgNoticia5} alt="" />
+                    <div role='group'>
+                        <h2>Confusão por assento em voo da Latam</h2>
+                        <p>Caso da passageira brasileira que foi obrigada a trocar de assento após cartão de embarque ser emitido com um assento diferente do que o que ela havia comprado levantou questionamentos</p>
+                    </div>
                 </article>
             </section>
         </section>
