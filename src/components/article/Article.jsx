35641cf55ffaeaece589018f8d53ac682ca85872
henriqu3x <163440449+henriqu3x@users.noreply.github.com>
2026-05-28 16:16:45 -0300

    melhoria de visibilidade dos article

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -4,7 +4,7 @@ import imgTest from '../../assets/noticia2.avif'
 const Article = (props) => {
     return (
         <a href="">
-            <article style={{backgroundImage:`url(${props.image})`}}>
+            <article style={{backgroundImage:`linear-gradient(rgba(0,0,0,0.9), rgba(0,0,0,0.2)), url(${props.image})`}}>
                 <h2>{props.title}</h2>
                 <p>{props.descricao}</p>
             </article>
