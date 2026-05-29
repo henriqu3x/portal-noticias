3ce94c6978066d3e14af8b160d2b87d28e214ef6
henriqu3x <163440449+henriqu3x@users.noreply.github.com>
2026-05-29 16:19:13 -0300

    refactor(cardNoticia): usando tag img, inves de usar no css

--- src/components/cardNoticia/CardNoticia.jsx
+++ src/components/cardNoticia/CardNoticia.jsx
@@ -3,8 +3,11 @@ import './cardNoticia.css'
 const CardNoticia = (props) => {
     return (
         <a aria-label='bloco de noticias' className='noticias-princi' href="">
-            <article style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.9), rgba(0,0,0,0.2)), url(${props.image})` }}>
-                <div>
+            <article>
+                <div className='img-wrraper'>
+                    <img src={props.image} alt={props.alt} />
+                </div>
+                <div className='content'>
                     <h2>{props.title}</h2>
                     <p>{props.descricao}</p>
                 </div>
