c54428e6a649f91f206393e171fb7672001d37ef
henriqu3x <163440449+henriqu3x@users.noreply.github.com>
2026-06-03 16:43:14 -0300

    feat(UserService): user services concluido

---
+++ backend/services/UserServices.js
@@ -0,0 +1,53 @@
+import prisma from "../prisma/client.js"
+
+class UsersServices {
+    async alterarStatus({id, status}){
+        const perfil = await prisma.perfil.findFirst({
+            where:{
+                tipo_perfil: status
+            }
+        })
+
+        if (!perfil) {
+            throw new Error("Perfil não encontrado");
+        }
+
+        const novoStatus = await prisma.usuario.update({
+            where:{
+                id: id
+            },
+            data:{
+                perfil_id: perfil.id
+            },
+            include:{
+                perfil: true
+            }
+        })
+
+        return {
+            id: novoStatus.id,
+            nome: novoStatus.nome,
+            tipo_perfil: novoStatus.perfil.tipo_perfil
+        }
+    }
+
+    async deletarUsuario(id){
+        const usuarioDeletado = await prisma.usuario.delete({
+            where:{
+                id:id
+            },
+            include:{
+                perfil:true
+            }
+        })
+
+        return {
+            id: usuarioDeletado.id,
+            nome: usuarioDeletado.nome,
+            tipo: usuarioDeletado.perfil.tipo_perfil,
+            message: "Usuario deletado com sucesso!"
+        }
+    }
+}
+
+export default UsersServices
