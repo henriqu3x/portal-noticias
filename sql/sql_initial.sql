869365e662a88c2eacd3001b2955a85ff028e9b0
henriqu3x <163440449+henriqu3x@users.noreply.github.com>
2026-06-03 14:09:27 -0300

    docs(SQL): criação do sql do banco

---
+++ sql/sql_initial.sql
@@ -0,0 +1,49 @@
+create extension if not exists pgcrypto;
+
+create table if not exists perfil(
+	id uuid primary key default gen_random_uuid(),
+	tipo_perfil varchar(20) not null default('cliente'),
+	check (tipo_perfil IN ('admin', 'cliente'))
+);
+
+create table if not exists usuario(
+	id uuid primary key default gen_random_uuid(),
+	nome varchar(75) not null,
+	email varchar(100) unique not null,
+	senha varchar(255) not null,
+	perfil_id uuid not null,
+	constraint fk_usuario_perfil foreign key (perfil_id) references perfil(id)
+);
+
+create table if not exists image(
+	id uuid primary key default gen_random_uuid(),
+	url text,
+	alt varchar(255)
+);
+
+create table if not exists noticia(
+	id uuid primary key default gen_random_uuid(),
+	titulo varchar(255) not null,
+	descricao text,
+	status boolean not null default(false),
+	image_id uuid,
+	data_publicacao timestamptz default(now()),
+	usuario_id uuid,
+	data_atualizacao timestamptz default(now()),
+	conteudo text not null,
+	constraint fk_noticia_image foreign key (image_id) references image(id),
+	constraint fk_noticia_usuario foreign key (usuario_id) references usuario(id)
+);
+
+create or replace function atualizar_data_atualizacao()
+returns trigger as $$
+begin
+	NEW.data_atualizacao = NOW();
+	return NEW;
+end;
+$$ language plpgsql;
+
+create trigger trg_noticia_data_atualizacao
+before update on noticia
+for each row
+execute function atualizar_data_atualizacao();
