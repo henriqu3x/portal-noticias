daca05bd69d4d65e7bca0635fc881c8683912d37
henriqu3x <163440449+henriqu3x@users.noreply.github.com>
2026-06-07 23:46:23 -0300

    refactor(registerScreen): tela de registro 100% finalizada e funcional

--- frontend/src/pages/RegisterScreen/RegisterScreen.jsx
+++ frontend/src/pages/RegisterScreen/RegisterScreen.jsx
@@ -1,10 +1,21 @@
 import { NavLink } from "react-router-dom";
 import "./registerScreen.css";
 import { useState } from "react";
+import {useAuth} from '../../context/AuthContext'
+import {useNavigate} from 'react-router-dom'
 
 const RegisterScreen = () => {
      const [showPass, setShowPass] = useState(false)
      const [showPassRepita, setShowPassRepita] = useState(false)
+
+     const [usuario, setUsuario] = useState('')
+     const [email, setEmail] = useState('')
+     const [senha, setSenha] = useState('')
+     const [repita_senha, setRepitaSenha] = useState('')
+     const [error, setError] = useState('')
+
+     const {register} = useAuth()
+     const navigate = useNavigate()
 
      const showPassword = () => {
           setShowPass(!showPass)
@@ -12,6 +23,20 @@ const RegisterScreen = () => {
 
      const showPasswordRepita = () => {
           setShowPassRepita(!showPassRepita)
+     }
+
+     const cadastar = async (e) => {
+        e.preventDefault()
+
+        try {
+          setError('')
+
+          const user = await register(usuario, email, senha, repita_senha)
+
+          navigate('/login')
+        } catch (error) {
+          error.message
+        }
      }
 
   return (
@@ -28,16 +53,22 @@ const RegisterScreen = () => {
           <h2>F5</h2>
           <h3>Cadastre-se para acessar todas as notícias disponíveis.</h3>
         </div>
-        <form>
+        <form onSubmit={cadastar}>
           <label htmlFor="usuario">Nome:</label>
-          <input required name="usuario" id="usuario" type="text" placeholder="Ex: Carlos" />
+          <input required name="usuario" id="usuario" type="text" placeholder="Ex: Carlos" 
+          onChange={(e) => setUsuario(e.target.value)}
+          />
 
           <label htmlFor="email">Email:</label>
-          <input required name="email" id="email" type="email" placeholder="Ex: example@email.com" />
+          <input required name="email" id="email" type="email" placeholder="Ex: example@email.com" 
+          onChange={(e) => setEmail(e.target.value)}
+          />
 
           <label htmlFor="senha">Senha:</label>
           <div className="input-senha">
-            <input required name="senha" id="senha" type={showPass?'text':'password'} placeholder="Ex: Carlos2090!" />
+            <input required name="senha" id="senha" type={showPass?'text':'password'} placeholder="Ex: Carlos2090!" 
+            onChange={(e) => setSenha(e.target.value)}
+            />
             <div className="box-eye" onClick={showPassword}>
               <i className="fa-solid fa-eye i-eye"></i>
             </div>
@@ -51,11 +82,14 @@ const RegisterScreen = () => {
               id="repita-senha"
               type={showPassRepita?'text':'password'}
               placeholder="Ex: Carlos2090!"
+              onChange={(e) => setRepitaSenha(e.target.value)}
             />
             <div className="box-eye" onClick={showPasswordRepita}>
                <i className="fa-solid fa-eye i-eye"></i>
             </div>
           </div>
+
+          {error ? error : null}
 
           <button aria-label="cadastrar" type="submit">
             Cadastrar
