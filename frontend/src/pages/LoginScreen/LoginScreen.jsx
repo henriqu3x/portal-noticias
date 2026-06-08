2f4bf3b0828e67a47f957193742f9dc18871b11b
henriqu3x <163440449+henriqu3x@users.noreply.github.com>
2026-06-07 23:31:19 -0300

    refactor(LoginScreen): login screen 100% terminado

--- frontend/src/pages/LoginScreen/LoginScreen.jsx
+++ frontend/src/pages/LoginScreen/LoginScreen.jsx
@@ -1,12 +1,39 @@
 import { NavLink } from 'react-router-dom'
 import './loginScreen.css'
 import { useState } from 'react'
+import {useAuth} from '../../context/AuthContext'
+import {useNavigate} from 'react-router-dom'
 
 const LoginScreen = () => {
     const [showPass, setShowPass] = useState(false)
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+    const [error, setError] = useState('')
+
+    const {login} = useAuth()
+    const navigate = useNavigate()
 
     const showPassword = () => {
         setShowPass(!showPass)
+    }
+
+    const fazerLogin = async (e) => {
+        e.preventDefault()
+
+        try {
+            setError('')
+            
+            const user = await login(email, senha)
+
+            if (user.tipo == 'admin') {
+                navigate('/admin')
+            } else {
+                navigate('/')
+            }
+
+        } catch (error) {
+            setError(error.message)
+        }
     }
 
     return (
@@ -17,17 +44,23 @@ const LoginScreen = () => {
                     <h2>F5</h2>
                     <h3>Faça login para acessar todas as notícias disponíveis.</h3>
                 </div>
-                <form>
+                <form onSubmit={fazerLogin}>
                     <label htmlFor="email">Email:</label>
-                    <input id='email' type="email" placeholder='Ex: example@email.com' />
+                    <input id='email' type="email" placeholder='Ex: example@email.com' 
+                    onChange={(e) => setEmail(e.target.value)}
+                    />
 
                     <label htmlFor="senha">Senha:</label>
                     <div className='input-senha'>
-                        <input id='senha' type={showPass ? 'text' : 'password'} placeholder='Ex: Carlos2090!' />
+                        <input id='senha' type={showPass ? 'text' : 'password'} placeholder='Ex: Carlos2090!' 
+                        onChange={(e) => setSenha(e.target.value)}
+                        />
                         <div onClick={showPassword} className='box-eye'>
                             <i className="fa-solid fa-eye i-eye"></i>
                         </div>
                     </div>
+
+                    {error? <p>{error}</p> : null}
 
                     <button aria-label='Fazer-Login' type='submit'>
                         Entrar
